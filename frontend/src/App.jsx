119d3c95155551331e5f4273840689264e5acc23
Giovanni Rosario Mauro <giovi.mauro03@gmail.com>
2024-05-07 21:02:13 +0200

    filtro per rating completato

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import Layout from './components/Layout.jsx'
 import Heading from './components/Heading.jsx'
 import MovieList from './components/MovieList.jsx'
 import MovieItem from './components/MovieItem.jsx'
-import Filter from './components/Filter.jsx'
+import Filters from './components/Filters.jsx'
 
 const App = props => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [filter, setFilter]= useState(false)
+  const [filterCheck, setFilterCheck]= useState(false)
+  const [filterRating, setFilterRating]= useState(false)
+  const [moviesRate, setMoviesRate]= useState([])
+  const rating= useRef('')
   
 
   const fetchMovies = () => {
@@ -19,42 +22,56 @@ const App = props => {
     return fetch('http://localhost:8000/movies')
       .then(response => response.json())
       .then(data => {
-        setMovies(data);
+        if(!filterCheck && !filterRating){
+          setMovies(data);
+        }
         setLoading(false);
       });
   }
 
   useEffect(() => {
     fetchMovies();
-  }, []);
+  }, [filterCheck, filterRating]);
 
 
+  // Checkbox recent
   function handleCheck(){
-      setFilter(prevFilter => !prevFilter ? true : false);
-  }
-  
-  let newMovies;
-  if(filter){
-    newMovies= movies.map((item)=> item ).sort((a, b) => b.year -  a.year)
+      setFilterCheck(prevFilter => !prevFilter ? true : false);
+      setMovies(prevMovies => prevMovies.sort((a, b) => b.year - a.year))
   }
 
+  // Input rating
+  function handleRate(){
+    if(rating.current.value !== ''){
+      setMoviesRate(movies.map((movie)=>movie).filter((movie) => Number(movie.rating) === Number(rating.current.value)))
+      setFilterRating(true);
+    }else {
+      setFilterRating(false)
+    }
+  }
+  // console.log('rate', movies);
+  // console.log('rating', rating.current.value);
+  // console.log('rateMovies', rateMovies);
+  // console.log('filter rate', filterRating);
 
-  console.log('filter', filter);
-  console.log('movies', movies);
   
 
   return (
     <Layout>
       <Heading/>
 
-      <Filter onCheck={handleCheck} />
-      <MovieList loading={loading}>
-        {!filter && movies.map((item, key) => (
+      <Filters 
+        onCheck={handleCheck} 
+        onRate={handleRate}
+        ref={rating} />
+      <MovieList noMovie={moviesRate.length === 0} loading={loading}>
+        {!filterRating && movies.map((item, key) => (
           <MovieItem key={key} {...item} />
         ))}
-        {filter && newMovies.map((item, key) => (
+        {filterRating && moviesRate.map((item, key) => (
           <MovieItem key={key} {...item} />
         ))}
+        {moviesRate.length === 0 && <p>Non ci sono film con questa valutazione</p>}
       </MovieList>
     </Layout>
   );
--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -1,16 +1,21 @@
 import { Spinner } from "flowbite-react";
 
-export default function MovieList({loading, children}){
-    if (loading) {
-        return (
-          <div className="text-center">
+export default function MovieList({loading, children, noMovie}){
+  if (loading) {
+    return (
+      <div className="text-center">
             <Spinner size="xl" />
           </div>
         );
       }
+      
+      let classDiv= "grid gap-4 md:gap-y-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3";
     
+      if(noMovie){
+        classDiv= "flex items-center justify-center";
+      }
       return (
-        <div className="grid gap-4 md:gap-y-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3">
+        <div className={classDiv}>
           {children}
         </div>
       );
